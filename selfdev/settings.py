d7c666b8a3ad243709b1f6a360960b9ad045ca30
subashyeager1801 <subashyeager18@gmail.com>
2026-08-11 20:24:15 +0530

    Fix Vercel static files and CSS routing with staticfiles_build

--- selfdev/settings.py
+++ selfdev/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # ---------------------------------------------------------------------------
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
